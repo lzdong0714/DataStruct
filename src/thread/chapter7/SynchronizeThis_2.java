6f2b691fae58aa28c02df13d3b0c8b0c9642986d
lzd <282139155@qq.com>
2019-05-28 23:25:12 +0800

    同步代码块 和 对象锁

---
+++ src/thread/chapter7/SynchronizeThis_2.java
@@ -0,0 +1,48 @@
+package thread.chapter7;
+
+public class SynchronizeThis_2 {
+
+    public static void main(String[] args) {
+        ThisLock_2 thisLock_2 = new ThisLock_2();
+        new Thread("T1"){
+            @Override
+            public void run() {
+                thisLock_2.m1();
+            }
+        }.start();
+
+        new Thread("T2"){
+            @Override
+            public void run() {
+                thisLock_2.m2();
+            }
+        }.start();
+    }
+}
+
+class ThisLock_2{
+
+    private final Object LOCK = new Object();
+
+    public synchronized void m1(){
+        System.out.println(Thread.currentThread().getName());
+
+        try {
+            Thread.sleep(10_000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void m2(){
+        synchronized (LOCK){
+            System.out.println(Thread.currentThread().getName());
+
+            try {
+                Thread.sleep(10_1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/thread/chapter7/TicketWindowRunable.java
@@ -0,0 +1,64 @@
+package thread.chapter7;
+
+public class TicketWindowRunable implements Runnable {
+
+    private final static int MAX = 500;
+
+    private int index = 1;
+
+//    //这里线程安全的
+//    public void run() {
+//        while (index <= MAX){
+//            System.out.println(Thread.currentThread().getName() +
+//                    " current number is : "+(index++));
+//            try {
+//            Thread.sleep(1);
+//            }catch (InterruptedException e){
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+
+    //这样是线程不安全的
+//    public void run(){
+//        while (true){
+//            if(index > MAX){
+//                break;
+//            }
+//            try {
+//                Thread.sleep(5);
+//            }catch (InterruptedException e){
+//                e.printStackTrace();
+//            }
+//
+//            System.out.println(Thread.currentThread().getName()+
+//                    " current number is : "+(index++)   );
+//
+//        }
+//    }
+
+
+    //同步代码块
+    private final Object MONITOR = new Object();
+
+    @Override
+    public void run() {
+        while (true){
+            synchronized (MONITOR){
+                if (index>MAX)
+                    break;
+
+                try {
+                    Thread.sleep(5);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+
+                System.out.println(Thread.currentThread().getName()+
+                        " current number is : "+(index++));
+            }
+        }
+    }
+
+
+}
