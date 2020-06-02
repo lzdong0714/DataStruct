f895455e8d1eac7d4ce985b2078dd5d8a64fb4d1
liuzhendong <952599783@qq.com>
2020-06-02 16:20:30 +0800

    is prime

--- src/data/struct/tree/RBTree.java
+++ src/data/struct/tree/RBTree.java
@@ -1,28 +1,35 @@
-package data.struct;
+package data.struct.tree;
 
 import data.struct.util.FileOperation;
 
 import java.util.ArrayList;
 
-public class BST<K extends Comparable<K>, V> {
+public class RBTree<K extends Comparable<K>, V> {
+
+    private static final boolean RED = true;
+    private static final boolean BLACK = false;
 
     private class Node{
         public K key;
         public V value;
         public Node left, right;
+        public boolean color;
+        int height;
 
         public Node(K key, V value){
             this.key = key;
             this.value = value;
             left = null;
             right = null;
+            color = RED;
+            height  =1;
         }
     }
 
     private Node root;
     private int size;
 
-    public BST(){
+    public RBTree(){
         root = null;
         size = 0;
     }
@@ -35,18 +42,87 @@ public class BST<K extends Comparable<K>, V> {
         return size == 0;
     }
 
-    // 向二分搜索树中添加新的元素(key, value)
-    public void add(K key, V value){
-        root = add(root, key, value);
+    // 判断节点node的颜色
+    private boolean isRed(Node node){
+        if(node == null)
+            return BLACK;
+        return node.color;
     }
 
-    // 向以node为根的二分搜索树中插入元素(key, value)，递归算法
-    // 返回插入新节点后二分搜索树的根
+    // 获得节点node的高度
+    private int getHeight(Node node){
+        if(node == null)
+            return 0;
+        return node.height;
+    }
+
+    // 对节点y进行向左旋转操作，返回旋转后新的根节点x
+    //    y                             x
+    //  /  \                          /   \
+    // T1   x      向左旋转 (y)       y     z
+    //     / \   - - - - - - - ->   / \   / \
+    //   T2  z                     T1 T2 T3 T4
+    //      / \
+    //     T3 T4
+    private Node leftRotate(Node y) {
+        Node x = y.right;
+        Node T2 = x.left;
+
+        // 向左旋转过程
+        x.left = y;
+        y.right = T2;
+
+        // 更新height
+        y.height = Math.max(getHeight(y.left), getHeight(y.right)) + 1;
+        x.height = Math.max(getHeight(x.left), getHeight(x.right)) + 1;
+
+        return x;
+    }
+
+    // 对节点y进行向右旋转操作，返回旋转后新的根节点x
+    //        y                              x
+    //       / \                           /   \
+    //      x   T4     向右旋转 (y)        z     y
+    //     / \       - - - - - - - ->    / \   / \
+    //    z   T3                       T1  T2 T3 T4
+    //   / \
+    // T1   T2
+    private Node rightRotate(Node y) {
+        Node x = y.left;
+        Node T3 = x.right;
+
+        // 向右旋转过程
+        x.right = y;
+        y.left = T3;
+
+        // 更新height
+        y.height = Math.max(getHeight(y.left), getHeight(y.right)) + 1;
+        x.height = Math.max(getHeight(x.left), getHeight(x.right)) + 1;
+
+        return x;
+    }
+
+    // 颜色翻转
+    private void flipColors(Node node){
+
+        node.color = RED;
+        node.left.color = BLACK;
+        node.right.color = BLACK;
+    }
+
+    // 向红黑树中添加新的元素(key, value)
+    public void add(K key, V value){
+        root = add(root, key, value);
+        root.color = BLACK; // 最终根节点为黑色节点
+    }
+
+    // 向以node为根的红黑树中插入元素(key, value)，递归算法
+    // 返回插入新节点后红黑树的根
     private Node add(Node node, K key, V value){
 
         if(node == null){
             size ++;
-            return new Node(key, value);
+            return new Node(key, value); // 默认插入红色节点，也是为了维护黑高
         }
 
         if(key.compareTo(node.key) < 0)
@@ -55,6 +131,15 @@ public class BST<K extends Comparable<K>, V> {
             node.right = add(node.right, key, value);
         else // key.compareTo(node.key) == 0
             node.value = value;
+        // case4怎么搞？
+        if (isRed(node.right) && !isRed(node.left))//这三个if的顺序不能换。
+            node = leftRotate(node);
+
+        if (isRed(node.left) && isRed(node.left.left))
+            node = rightRotate(node);
+
+        if (isRed(node.left) && isRed(node.right))
+            flipColors(node);
 
         return node;
     }
@@ -98,7 +183,7 @@ public class BST<K extends Comparable<K>, V> {
         return minimum(node.left);
     }
 
-    // 删除掉以node为根的二分搜索树中的最小节点
+     //删除掉以node为根的二分搜索树中的最小节点
     // 返回删除节点后新的二分搜索树的根
     private Node removeMin(Node node){
 
@@ -169,6 +254,33 @@ public class BST<K extends Comparable<K>, V> {
         }
     }
 
+    private Node moveRedLeft(Node node){
+        flipColors(node);
+        if(isRed(node.right.left)){
+            node.right= rightRotate(node.right);
+            node = leftRotate(node);
+        }
+        return node;
+    }
+
+    public void deleMin(){
+        if(!isRed(root.left)&&!isRed(root.right))
+            root.color = RED;
+        root = deleMin(root);
+        if(isEmpty()) root.color=BLACK;
+    }
+
+    private Node deleMin(Node node){
+        if(node.left == null)
+            return null;
+        if(isRed(node.left) &&isRed(node.left.left))
+            node = moveRedLeft(node);
+        node.left=deleMin(node.left);
+//        return balance(node);
+        return node;
+    }
+
+
     public static void main(String[] args){
 
         System.out.println("Pride and Prejudice");
@@ -177,7 +289,7 @@ public class BST<K extends Comparable<K>, V> {
         if(FileOperation.readFile("E:\\DataStruct\\src\\resource\\pride-and-prejudice.txt", words)) {
             System.out.println("Total words: " + words.size());
 
-            BST<String, Integer> map = new BST();
+            RBTree<String, Integer> map = new RBTree();
             for (String word : words) {
                 if (map.contains(word))
                     map.set(word, map.get(word) + 1);
@@ -187,9 +299,12 @@ public class BST<K extends Comparable<K>, V> {
 
             System.out.println("Total different words: " + map.getSize());
             System.out.println("Frequency of PRIDE: " + map.get("pride"));
-            System.out.println("Frequency of PREJUDICE: " + map.get("prejudice"));
-        }else {
+            int count = map.get("pride");
+            map.remove("pride");
+            System.out.println("Frequency of PRIDE: " + map.get("pride"));
+            System.out.println(map.getSize());
 
+            System.out.println("Frequency of PREJUDICE: " + map.get("prejudice"));
         }
 
         System.out.println();
